499fe6082131e68722078254b5b5b8313a595f67
Julia Sl <julia.slavkina@gmail.com>
2023-03-20 16:17:12 +0300

    исправление ошибки

--- task3.py
+++ task3.py
@@ -8,7 +8,7 @@ while i < n:
         count = int(input("Введите количество товара: "))
         measure = input("Введите единицу измерения товара: ")
         products.append(
-            (i, {'название': name, 'цена': price, 'количество': count, 'eд': measure}))
+            (i, {'название': name, 'цена': price, 'количество': count, 'единицы': measure}))
         i += 1
 print(products)
 
@@ -20,12 +20,12 @@ for i in products:
     subject_name.append(i[1].get('название'))
     subject_price.append(i[1].get('цена'))
     subject_count.append(i[1].get('количество'))
-    subject_measure.append(i[1].get('ед'))
-# print(subject_name)
-# print(subject_price)
-# print(subject_count)
-# print(subject_measure)
-analytics = ({'название': subject_name, 'цена': subject_price, 'количество': subject_count, 'ед': subject_measure})
+    subject_measure.append(i[1].get('единицы'))
+print(subject_name)
+print(subject_price)
+print(subject_count)
+print(subject_measure)
+analytics = ({'название': subject_name, 'цена': subject_price, 'количество': subject_count, 'единицы': subject_measure})
 print(analytics)
 
 # не смогла понять в чем ошибка, в той части где делается аналитика в список с единицами измерения записывается NONE (((
